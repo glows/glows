e882a0535ef301f8140b72008a2340b6edb14750
dongchuan <duncyun@gmail.com>
2021-07-02 16:26:38 +0800

    Update Content

---
+++ NEW copy.md
@@ -0,0 +1,19 @@
+<table>
+ <tr>
+
+  <tb valign="top" width="50%">
+<!-- douban starts -->
+
+- <a href='http://movie.douban.com/subject/24879258/' target='_blank'>[想看]非我吉日</a> - 2021-06-30
+- <a href='http://movie.douban.com/subject/30352607/' target='_blank'>[想看]坦白</a> - 2021-06-29
+- <a href='http://movie.douban.com/subject/10748120/' target='_blank'>[想看]真探 第一季</a> - 2021-06-29
+- <a href='http://movie.douban.com/subject/25735931/' target='_blank'>[想看]大西洋帝国 第五季</a> - 2021-06-29
+- <a href='http://movie.douban.com/subject/26926437/' target='_blank'>[想看]追缉：炸弹客 第一季</a> - 2021-06-29
+  <!-- douban ends -->
+  
+    </tb>
+    <tb valign="top" width="50%">
+    
+    </tb>
+   </tr>
+  </table>
--- build-readme.py
+++ build-readme.py
@@ -1,1 +1,77 @@
 import  json
+import feedparser
+import  datetime
+from time import mktime
+import email.utils
+import  pathlib
+import re
+
+root = pathlib.Path(__file__).parent.resolve()
+
+def formatGMTime(timestamp):
+    GMT_FORMAT = '%a, %d %b %Y %H:%M:%S GMT'
+    dateStr = datetime.datetime.strptime(timestamp, GMT_FORMAT) + datetime.timedelta(hours=8)
+    return dateStr.date()
+
+def formatRFC822Time(timestamp):
+    date_parsed = email.utils.parsedate(timestamp)
+    dt = datetime.datetime.fromtimestamp(mktime(date_parsed))
+    return dt.date()
+
+def replace_chunk(content, marker, chunk, inline=False):
+    r = re.compile(
+        r"<!\-\- {} starts \-\->.*<!\-\- {} ends \-\->".format(marker, marker),
+        re.DOTALL,
+    )
+    if not inline:
+        chunk = "\n{}\n".format(chunk)
+    chunk = "<!-- {} starts -->\n{}\n<!-- {} ends -->".format(marker, chunk, marker)
+    return r.sub(chunk, content)
+
+
+def fetch_douban():
+    entries = feedparser.parse("https://www.douban.com/feed/people/65855501/interests")["entries"]
+    return [
+        {
+            "title": "["+item["title"][0:2]+"]"+item["title"][2:],
+            "url": item["link"].split("#")[0],
+            "published": formatGMTime(item["published"])
+        }
+        for item in entries
+    ]
+
+
+def fetch_blog_entries():
+    entries = feedparser.parse("https://glows.github.io/index.xml")["entries"]
+    return [
+        {
+            "title": entry["title"],
+            "url": entry["link"].split("#")[0],
+            "published": formatRFC822Time(entry["published"]),
+        }
+        for entry in entries
+    ]
+    
+    
+if __name__ == "__main__":
+    readme = root / "NEW.md"
+    
+    doubans = fetch_douban()[:5]
+    entries = fetch_blog_entries()[:5]
+    doubans_md = "\n".join(
+        ["- <a href='{url}' target='_blank'>{title}</a> - {published}".format(**item) for item in
+         doubans]
+    )
+    
+    entries_md = "\n".join(
+        ["- <a href='{url}' target='_blank'>{title}</a> - {published}".format(**entry) for entry in
+         entries]
+    )
+    readme_contents = open(readme, encoding='utf-8').read()
+    rewritten = replace_chunk(readme_contents, "douban", doubans_md)
+    readme.open("w", encoding='utf-8').write(rewritten)
+    
+    
+    rewritten_blog = replace_chunk(rewritten, "blog", entries_md)
+    readme.open("w", encoding='utf-8',).write(rewritten_blog)
+    
